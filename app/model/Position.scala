a85dd8269acdfd6d06b975bd9b974368c8340ca8
Daniel Conde Garcia <einevea@gmail.com>
2017-07-06 23:33:47 +0100

    Correct comments from pull request

--- app/model/Position.scala
+++ app/model/Position.scala
@@ -6,5 +6,5 @@ import play.api.libs.json._
 case class Position(latitude: Double, longitude: Double, altitude: Double)
 
 object Position {
-  implicit val positionWriter = Json.writes[Position]
+  implicit val positionWriter: OWrites[Position] = Json.writes[Position]
 }
--- app/model/Sensor.scala
+++ app/model/Sensor.scala
@@ -1,6 +1,6 @@
 package model
 
-import play.api.libs.json.Json
+import play.api.libs.json.{Json, OWrites}
 
 case class MetricValue(id: String, value: Double, timestamp: Long)
 
@@ -9,8 +9,8 @@ case class Sensor(id: String, name: String)
 case class Device(id: String, name: String, position: Position, sensors: List[Sensor])
 
 object Sensor {
-  implicit val metricValueWriter = Json.writes[MetricValue]
-  implicit val sensorWriter = Json.writes[Sensor]
-  implicit val deviceWriter = Json.writes[Device]
+  implicit val metricValueWriter: OWrites[MetricValue] = Json.writes[MetricValue]
+  implicit val sensorWriter: OWrites[Sensor] = Json.writes[Sensor]
+  implicit val deviceWriter: OWrites[Device] = Json.writes[Device]
 }
         
